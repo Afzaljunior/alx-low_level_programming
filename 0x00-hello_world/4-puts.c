cbb643148d498dcbb538a763fd9abb6c83076477
Afzaljunior <afzaljunior02@gmail.com>
2023-03-16 14:36:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#includ < stdio.h >
+#include <stdio.h>
 /**
  * main -  A program that prints a line with puts fuction
-* Return:0 (success)
+* Return: 0 (success)
 */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	Return(0);
 }
